fdda63855825ab3c5904f2b39e4dc3ffee98dfa6
JesReyA <jesreyav@gmail.com>
2026-09-19 01:26:21 -0600

    Se añade grafica logaritmica para casos extremos o con mucha diferencia ej Burbuja vs Quicksort

--- graficos_metodos.py
+++ graficos_metodos.py
@@ -47,12 +47,42 @@ if __name__=="__main__":
     plt.grid()
     plt.show()
     
+    #Logaritmica
+    datos=pd.read_csv("burbuja_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("mezcla_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.yscale("symlog", linthresh=1e-4)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Burbuja Vs Mezcla")
+    plt.legend(('burbuja', 'mezcla'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
     datos=pd.read_csv("burbuja_tiempo.csv",sep=";")
     datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
     x=datos.N
     y=datos.Tiempo
     yy=datosDos.Tiempo
     plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Burbuja Vs Quicksort")
+    plt.legend(('burbuja', 'quicksort'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    #Comparacion con y de forma logaritmica en casos donde hay mucha diferencia y no se aprecia, se harán ambos tipos de grafica
+    datos=pd.read_csv("burbuja_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.yscale("symlog", linthresh=1e-4)
     plt.xlabel("N")
     plt.ylabel("Tiempo")
     plt.title("Burbuja Vs Quicksort")
@@ -86,12 +116,42 @@ if __name__=="__main__":
     plt.grid()
     plt.show()
     
+    #Logaritmica
+    datos=pd.read_csv("insercion_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("mezcla_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.yscale("symlog", linthresh=1e-4)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Insercion Vs Mezcla")
+    plt.legend(('insercion', 'mezcla'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
     datos=pd.read_csv("insercion_tiempo.csv",sep=";")
     datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
     x=datos.N
     y=datos.Tiempo
     yy=datosDos.Tiempo
     plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Insercion Vs Quicksort")
+    plt.legend(('insercion', 'quicksort'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    #Logaritmica
+    datos=pd.read_csv("insercion_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.yscale("symlog", linthresh=1e-4)
     plt.xlabel("N")
     plt.ylabel("Tiempo")
     plt.title("Insercion Vs Quicksort")
@@ -112,12 +172,42 @@ if __name__=="__main__":
     plt.grid()
     plt.show()
     
+    #Logaritmica
+    datos=pd.read_csv("seleccion_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("mezcla_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.yscale("symlog", linthresh=1e-4)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Seleccion Vs Mezcla")
+    plt.legend(('seleccion', 'mezcla'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
     datos=pd.read_csv("seleccion_tiempo.csv",sep=";")
     datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
     x=datos.N
     y=datos.Tiempo
     yy=datosDos.Tiempo
     plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Seleccion Vs Quicksort")
+    plt.legend(('seleccion', 'quicksort'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    #Logaritmica
+    datos=pd.read_csv("seleccion_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.yscale("symlog", linthresh=1e-4)
     plt.xlabel("N")
     plt.ylabel("Tiempo")
     plt.title("Seleccion Vs Quicksort")
