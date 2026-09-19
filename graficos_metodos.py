c8169765f1089a64b48ea1b5e838ad56ad44cd5f
JesReyA <jesreyav@gmail.com>
2026-09-19 01:09:12 -0600

    Se realizan todas las graficas juntas, pero falta ver si se puede hacer algo cuando hay mucha diferencia ya que se ve una linea recta por ejemplo Burbuja vs Quicksort

--- graficos_metodos.py
+++ graficos_metodos.py
@@ -20,3 +20,120 @@ if __name__=="__main__":
     plt.legend(('burbuja', 'insercion'),prop={'size':10},loc='upper right')
     plt.grid()
     plt.show()
+    
+    datos=pd.read_csv("burbuja_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("seleccion_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Burbuja Vs Seleccion")
+    plt.legend(('burbuja', 'seleccion'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    datos=pd.read_csv("burbuja_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("mezcla_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Burbuja Vs Mezcla")
+    plt.legend(('burbuja', 'mezcla'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    datos=pd.read_csv("burbuja_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Burbuja Vs Quicksort")
+    plt.legend(('burbuja', 'quicksort'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    datos=pd.read_csv("insercion_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("seleccion_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Insercion Vs Seleccion")
+    plt.legend(('insercion', 'seleccion'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    datos=pd.read_csv("insercion_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("mezcla_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Insercion Vs Mezcla")
+    plt.legend(('insercion', 'mezcla'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    datos=pd.read_csv("insercion_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Insercion Vs Quicksort")
+    plt.legend(('insercion', 'quicksort'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    datos=pd.read_csv("seleccion_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("mezcla_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Seleccion Vs Mezcla")
+    plt.legend(('seleccion', 'mezcla'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    datos=pd.read_csv("seleccion_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Seleccion Vs Quicksort")
+    plt.legend(('seleccion', 'quicksort'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
+    
+    datos=pd.read_csv("mezcla_tiempo.csv",sep=";")
+    datosDos=pd.read_csv("quicksort_tiempo.csv",sep=";")
+    x=datos.N
+    y=datos.Tiempo
+    yy=datosDos.Tiempo
+    plt.plot(x,y,x,yy)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Mezcla Vs Quicksort")
+    plt.legend(('mezcla', 'quicksort'),prop={'size':10},loc='upper right')
+    plt.grid()
+    plt.show()
